7deae1c0e0dce578546bcf5954e542c5b1ee2568
soap <hollynhudson2@gmail.com>
2026-01-14 04:08:04 -0600

    Unironically works ig

--- VCLYSS/obj/Debug/netstandard2.1/VCLYSS.AssemblyInfo.cs
+++ VCLYSS/obj/Debug/netstandard2.1/VCLYSS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("com.VCLYSS.sftwre")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68c65fc3c58e31e9b489fef6ddce2aa77a6a11d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbe2c01da1ab28bfe9126c0ae88f120d8a6e6657")]
 [assembly: System.Reflection.AssemblyProductAttribute("VCLYSS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VCLYSS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
