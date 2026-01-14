555b948142b1654f0c9d85c5062d1ee7e9c71739
soap <hollynhudson2@gmail.com>
2026-01-14 04:17:44 -0600

    i had a kernel panic LOL

--- VCLYSS/obj/Debug/netstandard2.1/VCLYSS.AssemblyInfo.cs
+++ VCLYSS/obj/Debug/netstandard2.1/VCLYSS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("com.VCLYSS.sftwre")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a639328d97aed54babf63f1e83a9cafd4c8b287d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b03523640fd2f51c4760ef7917b5aa33cc03681")]
 [assembly: System.Reflection.AssemblyProductAttribute("VCLYSS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VCLYSS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
